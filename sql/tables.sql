c09342db188240bfb83d7651fa50128ccf4fdd75
Jaime Vilorio Green <89463852+jaimeviloriogreen@users.noreply.github.com>
2022-05-26 17:13:34 -0400

    sql sintaxis

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE votantes(
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    sexo VARCHAR(10) NOT NULL
+    sexo VARCHAR(10) NOT NULL,
     cedula VARCHAR(11) NOT NULL
 );
 DROP TABLE IF EXISTS candidatos;
